8f65a7d71a0b072c5afdd6ea6454bd42bdb4a153
Mikel286 <aniamarinm@gmail.com>
2025-04-16 12:33:21 -0400

    upgrade v1.2

--- Python/Data analysis/main.py
+++ Python/Data analysis/main.py
@@ -8,6 +8,20 @@ columnas_utiles = ['latitud', 'longitud', 'comuna', 'region', 'cantidad_tonelada
 df_filtrado = df[columnas_utiles]
 
 # Guardar el nuevo archivo
-df_filtrado.to_csv("data/data filtering.csv", index=False)
+df_filtrado.to_csv("data/data_filtering.csv", index=False)
 
-print("✅ Archivo guardado como 'datos_filtrados.csv'")
+# Cargar nuevo CSV creado
+df = pd.read_csv("data/data_filtering.csv", sep=",", quotechar='"')
+
+# Limpiar columna de toneladas
+df['cantidad_toneladas'] = df['cantidad_toneladas'].astype(str).str.replace(",", ".").astype(float)
+
+# Agrupar: sumar toneladas y mantener la primera lat/lon/región por comuna
+resultado = df.groupby('comuna').agg({
+    'latitud': 'first',
+    'longitud': 'first',
+    'region': 'first',
+    'cantidad_toneladas': 'sum'
+}).reset_index()
+
+resultado.to_csv("data/general data.csv", index=False)
