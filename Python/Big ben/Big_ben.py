932411f13fb85743a366dbfdb2dd2f5bed11cfe7
unknown <aniamarinm@gmail.com>
2025-05-24 08:34:35 -0400

    proyect code update

--- Python/Big ben/Big_ben.py
+++ Python/Big ben/Big_ben.py
@@ -3,13 +3,8 @@ from time import sleep
 from machine import PWM, Pin
 
 reloj = servo360(6)
-x = 0
-
-while x < 100:
-
-    for y in range (1,101):
-        reloj.girar(90)
-        sleep(0.2)
-        reloj.detener()
-        x += 1
-        sleep(1)
+while True:
+    reloj.girar(70)
+    sleep(0.01)
+    reloj.detener()
+    sleep(1)
